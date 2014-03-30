50685944568931653d845855b2f7ecef0a1d241f
Wang Liang <beidawl1992@126.com>
2014-03-30 13:55:24 +0800

    add sample ruby on rails application

---
+++ Web-Application-Architecture/my_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyApp::Application.config.secret_key_base = 'b759ca6ea37b2fd6b8a20776e4bef4f1a0283d5f1377ce1a8ff42b1fa745437206534a712cbde1aae4fe6b8bd01ab4c54686cbbe946076020c641e18afd3466c'
