1d6f8fe48516a4ed809aa6a5d01e271f29a5076d
Wang Liang <beidawl1992@126.com>
2014-04-01 14:16:11 +0800

    update material

---
+++ Web-Application-Architecture/hw1/blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'fbce933f4ce78cb1e98a30ccfeae9a8289d985c484115c0fae189fe3f4af0c13bd72d671f49383dc2fe0f34407ad5851fef014bc5a9af60cab63e30cf2ec4443'
---
+++ Web-Application-Architecture/hw1/blog/db/migrate/20140401060012_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.integer :post_id
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
