c606ee8007797ba36afa7682262c80dc8df95201
wangliang <wangliangpeking@gmail.com>
2015-04-16 18:08:33 +0800

    Complete reactive week 1

--- Principles-of-Reactive-Programming/week1/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ Principles-of-Reactive-Programming/week1/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -9,9 +9,24 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  property("min1") = forAll { a: Int =>
+  property("min1") = forAll { a: Int =>    
     val h = insert(a, empty)
     findMin(h) == a
+  }
+  
+  property("min2") = forAll { (a:Int, b: Int) =>     
+     val h1 = insert(a, empty)
+     val h2 = insert(b, h1)
+     findMin(meld(h1, h2)) == Math.min(a, b)
+  }
+  
+  property("deleteMin") = forAll { (a: Int, b: Int, c: Int) =>
+    val h = insert(a, empty)
+    val h1 = insert(b, h)
+    val h2 = deleteMin(h1)
+    val h3 = deleteMin(meld(insert(c, empty), h1))
+    val h4 = deleteMin(h3)
+    findMin(h2) == Math.max(a, b) && findMin(h4) == Math.max(a, Math.max(b, c))
   }
 
   lazy val genHeap: Gen[H] = ???
