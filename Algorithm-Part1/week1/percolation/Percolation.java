fd5a213e64f5aca96b42c0c2c2ba9e27bbe9a983
wangliang <wangliangpeking@gmail.com>
2015-06-20 23:53:43 +0800

    Complete PA 1

--- Algorithm-Part1/week1/percolation/Percolation.java
+++ Algorithm-Part1/week1/percolation/Percolation.java
@@ -1,29 +1,86 @@
 
 public class Percolation {
 
+    private boolean[][] blocked;
+    private int row;
+    private int col;
+    private WeightedQuickUnionUF uf, aux;
+    private final int[] x = {0, 0, 1, -1};
+    private final int[] y = {-1, 1, 0, 0};
+    private int TOP;
+    private int BOTTOM;
+
     //  create N-by-N grid, with all sites blocked
     public Percolation(int N) {
-
+        if (N <= 0) {
+            throw new java.lang.IllegalArgumentException("Invalid arg");
+        }
+        uf = new WeightedQuickUnionUF(N * N + 2);
+        aux = new WeightedQuickUnionUF(N * N + 1);
+        blocked = new boolean[N][N];
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                blocked[i][j] = true;
+            }
+        }
+        row = N;
+        col = N;
+        TOP = 0;
+        BOTTOM = N * N + 1;
     }
 
     //  open site (row i, column j) if it is not open already
     public void open(int i, int j) {
-
+        check(i, j);
+        if (!blocked[i - 1][j - 1]) {
+            return;
+        }
+        int idx = (i - 1) * row + j;
+        if (i == 1) {
+            this.uf.union(TOP, idx);
+            this.aux.union(TOP, idx);
+        }
+        if (i == row) {
+            this.uf.union(idx, BOTTOM);
+        }
+        for (int k = 0; k < 4; ++k) {
+            int ni = i + x[k];
+            int nj = j + y[k];
+            if (ni <= 0 || ni > row
+                    || nj <= 0 || nj > col
+                    || blocked[ni - 1][nj - 1]) {
+                continue;
+            }
+            int nidx = (ni - 1) * row + nj;
+            this.uf.union(idx, nidx);
+            this.aux.union(idx, nidx);
+        }
+        blocked[i - 1][j - 1] = false;
+        return;
     }
 
     // is site (row i, column j) open?
     public boolean isOpen(int i, int j) {
-        return true;
+        check(i, j);
+        return !blocked[i - 1][j - 1];
     }
 
     // is site (row i, column j) full?
     public boolean isFull(int i, int j) {
-        return true;
+        check(i, j);
+        return !blocked[i - 1][j - 1] && aux.connected(TOP, (i - 1) * row + j);
     }
 
     // does the system percolate?
     public boolean percolates() {
-        return true;
+        return this.uf.connected(TOP, BOTTOM);
+    }
+
+    private void check(int i, int j) {
+        if (i <= 0 || i > row || j <= 0 || j > col) {
+            throw new java.lang.IndexOutOfBoundsException("Invalid argument");
+        }
+        return;
     }
 
     public static void main(String[] args) {
--- Algorithm-Part1/week1/percolation/PercolationStats.java
+++ Algorithm-Part1/week1/percolation/PercolationStats.java
@@ -1,34 +1,73 @@
 
 public class PercolationStats {
 
+    private double meanVal;
+    private double stddevVal;
+    private double confLow;
+    private double confHigh;
+
     // perform T independent experiments on an N-by-N grid
     public PercolationStats(int N, int T) {
-
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        double[] prob = new double[T];
+        int r, c, cnt;
+        for (int i = 0; i < T; ++i) {
+            Percolation perc = new Percolation(N);
+            cnt = 0;
+            while (!perc.percolates()) {
+                r = StdRandom.uniform(N) + 1;
+                c = StdRandom.uniform(N) + 1;
+                if (!perc.isOpen(r, c)) {
+                    perc.open(r, c);
+                    ++cnt;
+                }
+            }
+            prob[i] = 1.0 * cnt / (N * N);
+        }
+        this.meanVal = StdStats.mean(prob);
+        this.stddevVal = StdStats.stddev(prob);
+        this.confLow = meanVal - 1.96 * stddevVal / Math.sqrt(T);
+        this.confHigh = meanVal + 1.96 * stddevVal / Math.sqrt(T);
     }
 
     // sample mean of percolation threshold
     public double mean() {
-        return 0.0;
+        return this.meanVal;
     }
 
     // sample standard deviation of percolation threshold
     public double stddev() {
-        return 0.0;
+        return this.stddevVal;
     }
 
     // low  endpoint of 95% confidence interval
     public double confidenceLo() {
-        return 0.0;
+        return this.confLow;
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return 0.0;
+        return this.confHigh;
     }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
+        if (args.length < 2) {
+            StdOut.println("Two arguments needed: N T");
+            return;
+        }
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+        Stopwatch clock = new Stopwatch();
+        PercolationStats obj = new PercolationStats(N, T);
+        StdOut.println("Mean value: " + obj.mean());
+        StdOut.println("Stddev value: " + obj.stddev());
+        StdOut.println("95% confidence interval: " + obj.confidenceLo()
+                + " , " + obj.confidenceHi());
+        StdOut.println(clock.elapsedTime());
+        return;
     }
 
 }
