ae94754b02d1f86c107689638c7278087bb6c61d
wangliang <wangliangpeking@gmail.com>
2015-06-20 18:35:15 +0800

    Initialize algo hw 1

---
+++ Algorithm-Part1/week1/percolation/Percolation.java
@@ -0,0 +1,34 @@
+
+public class Percolation {
+
+    //  create N-by-N grid, with all sites blocked
+    public Percolation(int N) {
+
+    }
+
+    //  open site (row i, column j) if it is not open already
+    public void open(int i, int j) {
+
+    }
+
+    // is site (row i, column j) open?
+    public boolean isOpen(int i, int j) {
+        return true;
+    }
+
+    // is site (row i, column j) full?
+    public boolean isFull(int i, int j) {
+        return true;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        return;
+    }
+
+}
---
+++ Algorithm-Part1/week1/percolation/PercolationStats.java
@@ -0,0 +1,34 @@
+
+public class PercolationStats {
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T) {
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return 0.0;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return 0.0;
+    }
+
+    // low  endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return 0.0;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
