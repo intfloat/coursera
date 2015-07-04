ec1a9d9104b98697787f91e546a70111bc2174d2
wangliang <wangliangpeking@gmail.com>
2015-07-04 18:12:05 +0800

    Initialize HW 3

---
+++ Algorithm-Part1/week3/src/Brute.java
@@ -0,0 +1,9 @@
+
+public class Brute {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ Algorithm-Part1/week3/src/Fast.java
@@ -0,0 +1,9 @@
+
+public class Fast {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
