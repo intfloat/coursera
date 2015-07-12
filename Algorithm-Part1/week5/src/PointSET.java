579f47d96998e9523bef2872b498e77da957f25d
wangliang <wangliangpeking@gmail.com>
2015-07-13 00:35:23 +0800

    Add PA 5

---
+++ Algorithm-Part1/week5/src/PointSET.java
@@ -0,0 +1,48 @@
+
+public class PointSET {
+
+    // construct an empty set of points
+    public PointSET() {
+
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // number of points in the set
+    public int size() {
+        return 0;
+    }
+
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+
+    }
+
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        return false;
+    }
+
+    // draw all points to standard draw
+    public void draw() {
+
+    }
+
+    // all points that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) {
+        return null;
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        return null;
+    }
+
+    // unit testing of the methods (optional)
+    public static void main(String[] args) {
+
+    }
+}
